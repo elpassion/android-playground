776aa0dfc3e7ced939e626e679d449efa6282fe7
ghostbuster91 <kghost0@gmail.com>
2016-07-11 15:26:27 +0200

    testEmptyPlaceholderShouldBeVisibleOnCreate

--- app/src/androidTest/java/com/example/elpassion/githubtester/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/elpassion/githubtester/MainActivityTest.kt
@@ -1,5 +1,9 @@
 package com.example.elpassion.githubtester
 
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import org.junit.Rule
 import org.junit.Test
@@ -13,5 +17,10 @@ class MainActivityTest {
     @Test
     fun testShouldStartMainActivity() {
 
+    }
+
+    @Test
+    fun testEmptyPlaceholderShouldBeVisibleOnCreate() {
+        onView(withId(R.id.empty_placeholder)).check(matches(isDisplayed()))
     }
 }
--- app/src/main/java/com/example/elpassion/githubtester/MainActivity.kt
+++ app/src/main/java/com/example/elpassion/githubtester/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.elpassion.githubtester
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.example.elpassion.githubtester.search.GitHubApi
+import com.example.elpassion.githubtester.search.GitHubResultsView
+import com.example.elpassion.githubtester.search.GitHubSearchController
+import com.example.elpassion.githubtester.search.Users
+import rx.Observable
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GitHubResultsView {
+    override fun showEmptyListPlaceholder() {
+        findViewById(R.id.empty_placeholder).visibility = View.VISIBLE
+    }
+
+    override fun showResults(users: Users) {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showError(throwable: Throwable) {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    val controller = GitHubSearchController(this, object : GitHubApi {
+        override fun call(query: String): Observable<Users> {
+            throw RuntimeException()
+        }
+
+    })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        controller.onCreate()
     }
 }
